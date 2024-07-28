820d960d851674f219e420cd23dd6bb2f368e8bb
IrinaTescu <irina.tescu@outlook.com>
2024-07-28 17:48:10 +0300

    Extract class for copy

--- FolderSynchronizer/FolderSynchronizerApp/Program.cs
+++ FolderSynchronizer/FolderSynchronizerApp/Program.cs
@@ -6,33 +6,12 @@
         {
             string sourceFolderPath = @"C:\Playground\Source";
             string replicaFolderPath = @"C:\Playground\Replica";
-
-            var sourceFiles = Directory.GetFiles(sourceFolderPath,"*",SearchOption.AllDirectories);
-            var replicaFiles = Directory.GetFiles(replicaFolderPath,"*",SearchOption.AllDirectories);
-
-            foreach(var sourceFile in sourceFiles)
-            {
-                var additionalPath=Path.GetDirectoryName(sourceFile).Substring(sourceFolderPath.Length);
-                if(additionalPath.StartsWith(Path.DirectorySeparatorChar))
-                {
-
-                   additionalPath=additionalPath.Substring(1);
-                }
-                var destinationPath = Path.Combine(replicaFolderPath,additionalPath, Path.GetFileName(sourceFile));
-                if (!replicaFiles.Contains(destinationPath))
-                {
-                    if(!Directory.Exists(Path.GetDirectoryName(destinationPath)))
-                    {
-
-                       Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
-                    }
-                    File.Copy(sourceFile, destinationPath);
-                }
-            }
-            
-            
-            
+            var sourceToReplicaFileCopier = new SourceToReplicaFileCopier(sourceFolderPath, replicaFolderPath);
+            sourceToReplicaFileCopier.Execute();
 
         }
+
+        
     }
 }
+
---
+++ FolderSynchronizer/FolderSynchronizerApp/SourceToReplicaFileCopier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSynchronizerApp
+{
+    internal class SourceToReplicaFileCopier
+    {
+        private readonly string _sourceFolderPath;
+        private readonly string _replicaFolderPath;
+        public SourceToReplicaFileCopier(string sourceFolderPath, string replicaFolderPath)
+        {
+            _sourceFolderPath = sourceFolderPath;
+            _replicaFolderPath = replicaFolderPath;
+        }
+
+        public void Execute()
+        {
+            var sourceFiles = Directory.GetFiles(_sourceFolderPath, "*", SearchOption.AllDirectories);
+            var replicaFiles = Directory.GetFiles(_replicaFolderPath, "*", SearchOption.AllDirectories);
+
+            foreach (var sourceFile in sourceFiles)
+            {
+                var additionalPath = Path.GetDirectoryName(sourceFile).Substring(_sourceFolderPath.Length);
+                if (additionalPath.StartsWith(Path.DirectorySeparatorChar))
+                {
+
+                    additionalPath = additionalPath.Substring(1);
+                }
+                var destinationPath = Path.Combine(_replicaFolderPath, additionalPath, Path.GetFileName(sourceFile));
+                if (!replicaFiles.Contains(destinationPath))
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(destinationPath)))
+                    {
+
+                        Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+                    }
+                    File.Copy(sourceFile, destinationPath);
+                }
+            }
+        }
+    }
+}
