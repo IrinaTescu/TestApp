954ecb26a52a4ec53265f668022c6144850c3321
IrinaTescu <irina.tescu@outlook.com>
2024-07-30 15:19:08 +0300

    Validate input args

--- FolderSynchronizer/FolderSynchronizerApp/Program.cs
+++ FolderSynchronizer/FolderSynchronizerApp/Program.cs
@@ -5,10 +5,38 @@
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
-            string sourceFolderPath = @"C:\Playground\Source";
-            string replicaFolderPath = @"C:\Playground\Replica";
-            Logger.Info("Copy files from {Source} to {Replica}",sourceFolderPath,replicaFolderPath);
-            var sourceToReplicaFolder=new SourceToReplicaFolderCopier(sourceFolderPath,replicaFolderPath);
+            if (args.Length != 3)
+            {
+                Logger.Error("Please provide the parameters: <source> <replica> <syncInterval in minutes>");
+                return;
+            }
+
+            string sourceFolderPath = args[0];
+            if (!Directory.Exists(sourceFolderPath))
+            {
+                Logger.Error("Source folder does not exist");
+                return;
+            }
+            string replicaFolderPath = args[1];
+            if (!Directory.Exists(replicaFolderPath))
+            {
+                Logger.Error("Replica folder does not exist");
+                return;
+            }
+            if(int.TryParse(args[2], out int syncInterval))
+            {
+                Logger.Error("Sync interval must be a number>0");
+                return;
+            }
+            if (syncInterval <= 0)
+            {
+                Logger.Error("Sync interval should be greater than 0");
+                return;
+            }
+
+
+            Logger.Info("Copy files from {Source} to {Replica}", sourceFolderPath, replicaFolderPath);
+            var sourceToReplicaFolder = new SourceToReplicaFolderCopier(sourceFolderPath, replicaFolderPath);
             var sourceToReplicaFileCopier = new SourceToReplicaFileCopier(sourceFolderPath, replicaFolderPath, new MD5Calculator());
             sourceToReplicaFolder.Execute();
             sourceToReplicaFileCopier.Execute();
@@ -22,7 +50,7 @@
 
         }
 
-        
+
     }
 }
 
